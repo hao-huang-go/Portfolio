1e34b07d3e6c7cc2589f9029e9c60728f96b2867
haohuang <haohuang0507@gmail.com>
2024-11-15 16:02:46 +1100

    Fix Resume out of width limit

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -33,8 +33,8 @@ const ResumeNew: React.FC = () => {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.4 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.4: 0.6} />
           </Document>
         </Row>
 
